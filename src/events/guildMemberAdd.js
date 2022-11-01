35e533d904e72cb1bb1a3936141d836360050026
Shubham Jaiswal <shubhamjaiswal27@github.com>
2022-11-01 23:28:02 +0530

    update the event processor

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,8 +1,7 @@
 import { EmbedBuilder, userMention } from "discord.js";
-import axios from "axios";
 
 const CHANNEL_NAME = process.env.CHANNEL_NAME;
-const MEME_API = process.env.MEME_API_URL;
+const MEME_URL = process.env.MEME_URL || "";
 
 const event = {
   name: "guildMemberAdd",
@@ -11,30 +10,50 @@ const event = {
       (channel) => channel.name === CHANNEL_NAME
     );
 
-    const welcomeMessage = await getWelcomeMessageWithMeme(member.id);
+    /*
+      TODO: Change getWelcomeMessage to getWelcomeMessageWithMeme to send a meme to welcome your user.
+    */
+    const welcomeMessage = await getWelcomeMessage(member.id);
     channel.send(welcomeMessage);
   },
 };
 
-// this function returns a welcome message.
 const getWelcomeMessage = (userId) => {
-  return { content: `Welcome to the server, ${userMention(userId)}!` };
+  /*
+    this function returns a welcome message.
+    Play around with the code here and customise the welcome message.
+  */
+  return {
+    content: `Welcome ${userMention(userId)},
+    Hope you have great time here!
+  `,
+  };
 };
 
-// this function returns a welcome message with a meme.
+//
 const getWelcomeMessageWithMeme = async (userId) => {
+  /*
+    this function returns a welcome message with a meme.
+    Play around with the code here and customise the welcome message.
+
+    TODO: Change this function to return different welcome message with a meme everytime a new user joins.
+  */
   const meme = await getMeme();
 
   return {
-    content: `Welcome to the server, ${userMention(userId)}!
-    Here's a meme for you!`,
+    content: `Welcome ${userMention(userId)},
+    Here's a meme for you to enjoy!`,
     embeds: [meme],
   };
 };
 
 const getMeme = async () => {
-  const response = await axios.get(MEME_API);
-  return new EmbedBuilder().setImage(response.data.url);
+  /*
+    this function returns a meme.
+
+    TODO: change this function to return a different meme randomly everytime a new user joins.
+  */
+  return new EmbedBuilder().setImage(MEME_URL);
 };
 
 export default event;
